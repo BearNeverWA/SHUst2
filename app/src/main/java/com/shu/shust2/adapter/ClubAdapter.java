abac4a82013e44f3ef592758c9de33614891061f
Leo <915968268@qq.com>
2017-09-05 21:20:53 +0800

    添加了列表懒加载

--- app/src/main/java/com/shu/shust2/adapter/ClubAdapter.java
+++ app/src/main/java/com/shu/shust2/adapter/ClubAdapter.java
@@ -2,15 +2,18 @@ package com.shu.shust2.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+
 import com.bumptech.glide.Glide;
 import com.shu.shust2.R;
-import com.shu.shust2.activity.MainActivity;
 import com.shu.shust2.model.Club;
 
 import java.util.List;
@@ -19,24 +22,117 @@ import java.util.List;
  * Created by Leo on 2017/9/4.
  */
 
-public class ClubAdapter extends RecyclerView.Adapter<ClubAdapter.ViewHolder> {
+public class ClubAdapter extends Adapter<ViewHolder> {
 
     private Context mContext;
-    private List<Club> clubList;
+    private List clubList;
 
-    private final static int TYPE_ITEM=0;
-    private final static int TYPE_FOOT=1;
-    
+    private final static int TYPE_ITEM = 0;
+    private final static int TYPE_FOOT = 1;
 
-    static class ViewHolder extends RecyclerView.ViewHolder {
 
+    public ClubAdapter(List clubs) {
+        clubList = clubs;
+    }
+
+    @Override
+    public int getItemCount() {
+        Log.d("test", "getItemCount: " + clubList);
+        return clubList.size() == 0 ? 0 : clubList.size() + 1;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position + 1 == getItemCount())
+            return TYPE_FOOT;
+        else
+            return TYPE_ITEM;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (mContext == null)
+            mContext = parent.getContext();
+        if (viewType == TYPE_ITEM) {
+            View view = LayoutInflater.from(mContext).inflate(R.layout.item_club, parent, false);
+            return new ItemViewHolder(view);
+        } else if (viewType == TYPE_FOOT) {
+            View view = LayoutInflater.from(mContext).inflate(R.layout.item_foot, parent, false);
+            return new FootViewHolder(view);
+        }
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        if (holder instanceof ItemViewHolder) {
+            int pos = holder.getLayoutPosition();
+            Club club = (Club) clubList.get(position);
+            Glide.with(mContext).load(club.getClubLogo()).into(((ItemViewHolder) holder).clubLogo);
+            ((ItemViewHolder) holder).clubName.setText(club.getClubName());
+            switch (club.getClubType()) {
+                case "学术科技":
+                    ((ItemViewHolder) holder).clubType.setBackgroundResource(R.drawable.blue_border);
+                    ((ItemViewHolder) holder).clubType.setTextColor(mContext.getResources().getColor(R.color.soft_blue));
+                    break;
+                case "体育健身":
+                    ((ItemViewHolder) holder).clubType.setBackgroundResource(R.drawable.green_border);
+                    ((ItemViewHolder) holder).clubType.setTextColor(mContext.getResources().getColor(R.color.soft_green));
+                    break;
+                case "公益实践":
+                    ((ItemViewHolder) holder).clubType.setBackgroundResource(R.drawable.orange_border);
+                    ((ItemViewHolder) holder).clubType.setTextColor(mContext.getResources().getColor(R.color.orange));
+                    break;
+                case "文化艺术":
+                    ((ItemViewHolder) holder).clubType.setBackgroundResource(R.drawable.red_border);
+                    ((ItemViewHolder) holder).clubType.setTextColor(mContext.getResources().getColor(R.color.red));
+                    break;
+                case "社会科学":
+                    ((ItemViewHolder) holder).clubType.setBackgroundResource(R.drawable.pink_round);
+                    ((ItemViewHolder) holder).clubType.setTextColor(mContext.getResources().getColor(R.color.pink));
+                    break;
+                case "理论学习":
+                    ((ItemViewHolder) holder).clubType.setBackgroundResource(R.drawable.purple_border);
+                    ((ItemViewHolder) holder).clubType.setTextColor(mContext.getResources().getColor(R.color.purple));
+                    break;
+                default:
+                    break;
+            }
+            ((ItemViewHolder) holder).clubType.setText(club.getClubType());
+            String star;
+            switch (club.getClubStar()) {
+                case 1:
+                    star = "一星级";
+                    break;
+                case 2:
+                    star = "两星级";
+                    break;
+                case 3:
+                    star = "三星级";
+                    break;
+                case 4:
+                    star = "四星级";
+                    break;
+                case 5:
+                    star = "五星级";
+                    break;
+                default:
+                    star = "无";
+                    break;
+            }
+            ((ItemViewHolder) holder).clubStar.setText(star);
+            ((ItemViewHolder) holder).clubIntro.setText(club.getClubIntro());
+        }
+    }
+
+    static class ItemViewHolder extends ViewHolder {
         ImageView clubLogo;
         TextView clubName;
         TextView clubType;
         TextView clubStar;
         TextView clubIntro;
 
-        public ViewHolder(View itemView) {
+        public ItemViewHolder(View itemView) {
             super(itemView);
             clubLogo = itemView.findViewById(R.id.iv_club_logo);
             clubName = itemView.findViewById(R.id.tv_club_name);
@@ -46,81 +142,10 @@ public class ClubAdapter extends RecyclerView.Adapter<ClubAdapter.ViewHolder> {
         }
     }
 
-    public ClubAdapter(List<Club> clubs) {
-        clubList = clubs;
-    }
+    static class FootViewHolder extends ViewHolder {
 
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        if (mContext == null)
-            mContext = parent.getContext();
-        View view = LayoutInflater.from(mContext).inflate(R.layout.item_club, parent, false);
-        return new ViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        Club club = clubList.get(position);
-        Glide.with(mContext).load(club.getClubLogo()).into(holder.clubLogo);
-        holder.clubName.setText(club.getClubName());
-
-        switch (club.getClubType()) {
-            case "学术科技":
-                holder.clubType.setBackgroundResource(R.drawable.blue_border);
-                holder.clubType.setTextColor(mContext.getResources().getColor(R.color.soft_blue));
-                break;
-            case "体育健身":
-                holder.clubType.setBackgroundResource(R.drawable.green_border);
-                holder.clubType.setTextColor(mContext.getResources().getColor(R.color.soft_green));
-                break;
-            case "公益实践":
-                holder.clubType.setBackgroundResource(R.drawable.orange_border);
-                holder.clubType.setTextColor(mContext.getResources().getColor(R.color.orange));
-                break;
-            case "文化艺术":
-                holder.clubType.setBackgroundResource(R.drawable.red_border);
-                holder.clubType.setTextColor(mContext.getResources().getColor(R.color.red));
-                break;
-            case "社会科学":
-                holder.clubType.setBackgroundResource(R.drawable.pink_round);
-                holder.clubType.setTextColor(mContext.getResources().getColor(R.color.pink));
-                break;
-            case "理论学习":
-                holder.clubType.setBackgroundResource(R.drawable.purple_border);
-                holder.clubType.setTextColor(mContext.getResources().getColor(R.color.purple));
-                break;
-            default:
-                break;
+        public FootViewHolder(View itemView) {
+            super(itemView);
         }
-        holder.clubType.setText(club.getClubType());
-
-        String star;
-        switch (club.getClubStar()) {
-            case 1:
-                star = "一星级";
-                break;
-            case 2:
-                star = "两星级";
-                break;
-            case 3:
-                star = "三星级";
-                break;
-            case 4:
-                star = "四星级";
-                break;
-            case 5:
-                star = "五星级";
-                break;
-            default:
-                star = "无";
-                break;
-        }
-        holder.clubStar.setText(star);
-        holder.clubIntro.setText(club.getClubIntro());
-    }
-
-    @Override
-    public int getItemCount() {
-        return clubList.size();
     }
 }
--- app/src/main/java/com/shu/shust2/fragment/ClubFragment.java
+++ app/src/main/java/com/shu/shust2/fragment/ClubFragment.java
@@ -1,6 +1,7 @@
 package com.shu.shust2.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
@@ -27,39 +28,66 @@ import java.util.Random;
 
 public class ClubFragment extends Fragment {
 
-    private List<Club> clubs;
+    private List<Club> clubs = new ArrayList<>();
     private Club club;
-    private ClubAdapter adapter;
+    private ClubAdapter adapter = new ClubAdapter(clubs);
     private String[] types = {"学术科技", "体育健身", "公益实践", "文化艺术", "社会科学", "理论学习"};
+    private Handler handler = new Handler();
+
+    boolean isLoading;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_club, container, false);
-        initData();
 
-        //导航栏
+        //导航栏i
         Toolbar toolbar = view.findViewById(R.id.toolbar_club);
         ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
         ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("");
 
         //社团list
         RecyclerView recyclerView = view.findViewById(R.id.rv_club);
-        LinearLayoutManager manager = new LinearLayoutManager(getActivity());
+        final LinearLayoutManager manager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(manager);
-        adapter = new ClubAdapter(clubs);
         recyclerView.setAdapter(adapter);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                int lastVisibleItemPosition = manager.findLastVisibleItemPosition();
+                if (lastVisibleItemPosition + 1 == adapter.getItemCount()) {
+                    if (!isLoading) {
+                        isLoading = true;
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                initData();
+                                isLoading = false;
+                            }
+                        }, 1000);
+                    }
+                }
+            }
+        });
+        initData();
 
         return view;
     }
 
     private void initData() {
-        clubs = new ArrayList<>();
         Random random = new Random();
         for (int i = 0; i < 10; i++) {
             club = new Club(R.mipmap.ic_launcher, "测试" + i, types[random.nextInt(5)], random.nextInt(4) + 1, "测试介绍" + i);
             clubs.add(club);
         }
+        adapter.notifyDataSetChanged();
+        adapter.notifyItemRemoved(adapter.getItemCount());
     }
 
 }
