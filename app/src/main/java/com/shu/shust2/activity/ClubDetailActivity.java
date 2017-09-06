42b22ad67d312cf3a0221c4141fa7a92f34dae9f
Leo <915968268@qq.com>
2017-09-06 10:48:55 +0800

    nothing

--- app/src/main/java/com/shu/shust2/activity/ClubDetailActivity.java
+++ app/src/main/java/com/shu/shust2/activity/ClubDetailActivity.java
@@ -2,6 +2,7 @@ package com.shu.shust2.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
 import com.shu.shust2.R;
@@ -12,6 +13,8 @@ public class ClubDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_club_detail);
+
+
 
         Toast.makeText(this, getIntent().getStringExtra("name"), Toast.LENGTH_SHORT).show();
     }
